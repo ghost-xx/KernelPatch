53f29cdcdeb3e7910972ac01c24c2238ddc8d0b1
ghost-xx <wen55444@gmail.com>
2025-09-15 00:05:56 +0800

    回档

--- kpms/demo-syscallhook/syscallhook.c
+++ kpms/demo-syscallhook/syscallhook.c
@@ -13,9 +13,6 @@
 #include <linux/string.h>
 #include <kputils.h>
 #include <asm/current.h>
-#include <linux/sched.h>
-#include <linux/sched/task.h>
-#include <linux/kernel.h>
 
 KPM_NAME("kpm-syscall-hook-demo");
 KPM_VERSION("1.0.0");
@@ -26,7 +23,16 @@ KPM_DESCRIPTION("KernelPatch Module System Call Hook Example");
 const char *margs = 0;
 enum hook_type hook_type = NONE;
 
-// 这些定义已经在 linux/sched.h 中存在，不需要重复定义
+enum pid_type
+{
+    PIDTYPE_PID,
+    PIDTYPE_TGID,
+    PIDTYPE_PGID,
+    PIDTYPE_SID,
+    PIDTYPE_MAX,
+};
+struct pid_namespace;
+pid_t (*__task_pid_nr_ns)(struct task_struct *task, enum pid_type type, struct pid_namespace *ns) = 0;
 
 void before_openat_0(hook_fargs4_t *args, void *udata)
 {
@@ -40,15 +46,15 @@ void before_openat_0(hook_fargs4_t *args, void *udata)
 
     struct task_struct *task = current;
     pid_t pid = -1, tgid = -1;
-    
-    // 使用内核提供的函数获取PID和TGID
-    pid = __task_pid_nr_ns(task, PIDTYPE_PID, 0);
-    tgid = __task_pid_nr_ns(task, PIDTYPE_TGID, 0);
+    if (__task_pid_nr_ns) {
+        pid = __task_pid_nr_ns(task, PIDTYPE_PID, 0);
+        tgid = __task_pid_nr_ns(task, PIDTYPE_TGID, 0);
+    }
 
     args->local.data0 = (uint64_t)task;
 
-    // 输出详细信息 (PID开头便于过滤)
-    printk(KERN_INFO "[KP] pid: %d, tgid: %d, task: %llx, openat dfd: %d, filename: %s, flag: %x, mode: %d\n", 
+    // PID开头的日志格式，便于dmesg过滤
+    printk(KERN_INFO "[KP] pid:%d tgid:%d task:%llx openat dfd:%d filename:%s flag:%x mode:%d\n", 
            pid, tgid, task, dfd, buf, flag, mode);
 }
 
@@ -58,21 +64,21 @@ void before_openat_1(hook_fargs4_t *args, void *udata)
 {
     uint64_t *pcount = (uint64_t *)udata;
     (*pcount)++;
-    printk(KERN_INFO "[KP] hook_chain_1 before openat task: %llx, count: %llx\n", args->local.data0, *pcount);
+    printk(KERN_INFO "[KP] hook_chain_1 before openat task:%llx count:%llx\n", args->local.data0, *pcount);
 }
 
 void after_openat_1(hook_fargs4_t *args, void *udata)
 {
-    printk(KERN_INFO "[KP] hook_chain_1 after openat task: %llx\n", args->local.data0);
+    printk(KERN_INFO "[KP] hook_chain_1 after openat task:%llx\n", args->local.data0);
 }
 
 static long syscall_hook_demo_init(const char *args, const char *event, void *__user reserved)
 {
     margs = args;
-    printk(KERN_INFO "[KP] kpm-syscall-hook-demo init ..., args: %s\n", margs);
+    printk(KERN_INFO "[KP] kpm-syscall-hook-demo init, args:%s\n", margs ? margs : "null");
 
-    // __task_pid_nr_ns 函数已经通过头文件可用，不需要动态查找
-    printk(KERN_INFO "[KP] kernel function __task_pid_nr_ns available via headers\n");
+    __task_pid_nr_ns = (typeof(__task_pid_nr_ns))kallsyms_lookup_name("__task_pid_nr_ns");
+    printk(KERN_INFO "[KP] kernel function __task_pid_nr_ns addr:%llx\n", __task_pid_nr_ns);
 
     if (!margs) {
         printk(KERN_WARNING "[KP] no args specified, skip hook\n");
@@ -82,23 +88,23 @@ static long syscall_hook_demo_init(const char *args, const char *event, void *__
     hook_err_t err = HOOK_NO_ERR;
 
     if (!strcmp("function_pointer_hook", margs)) {
-        printk(KERN_INFO "[KP] function pointer hook ...\n");
+        printk(KERN_INFO "[KP] function pointer hook enabled\n");
         hook_type = FUNCTION_POINTER_CHAIN;
         err = fp_hook_syscalln(__NR_openat, 4, before_openat_0, 0, 0);
         if (err) goto out;
         err = fp_hook_syscalln(__NR_openat, 4, before_openat_1, after_openat_1, &open_counts);
     } else if (!strcmp("inline_hook", margs)) {
-        printk(KERN_INFO "[KP] inline hook ...\n");
+        printk(KERN_INFO "[KP] inline hook enabled\n");
         hook_type = INLINE_CHAIN;
         err = inline_hook_syscalln(__NR_openat, 4, before_openat_0, 0, 0);
     } else {
-        printk(KERN_WARNING "[KP] unknown args: %s\n", margs);
+        printk(KERN_WARNING "[KP] unknown args:%s\n", margs);
         return 0;
     }
 
 out:
     if (err) {
-        printk(KERN_ERR "[KP] hook openat error: %d\n", err);
+        printk(KERN_ERR "[KP] hook openat error:%d\n", err);
     } else {
         printk(KERN_INFO "[KP] hook openat success\n");
     }
@@ -107,7 +113,7 @@ out:
 
 static long syscall_hook_control0(const char *args, char *__user out_msg, int outlen)
 {
-    printk(KERN_INFO "[KP] syscall_hook control, args: %s\n", args);
+    printk(KERN_INFO "[KP] syscall_hook control, args:%s\n", args ? args : "null");
     
     if (!args) {
         printk(KERN_WARNING "[KP] control: no args specified\n");
@@ -117,9 +123,9 @@ static long syscall_hook_control0(const char *args, char *__user out_msg, int ou
     hook_err_t err = HOOK_NO_ERR;
     
     if (!strcmp("function_pointer_hook", args)) {
-        printk(KERN_INFO "[KP] control: setting up function pointer hook ...\n");
+        printk(KERN_INFO "[KP] control: enabling function pointer hook\n");
         
-        // 如果已经有钩子，先清理
+        // 清理已有钩子
         if (hook_type == FUNCTION_POINTER_CHAIN) {
             fp_unhook_syscalln(__NR_openat, before_openat_0, 0);
             fp_unhook_syscalln(__NR_openat, before_openat_1, after_openat_1);
@@ -133,9 +139,9 @@ static long syscall_hook_control0(const char *args, char *__user out_msg, int ou
         err = fp_hook_syscalln(__NR_openat, 4, before_openat_1, after_openat_1, &open_counts);
         
     } else if (!strcmp("inline_hook", args)) {
-        printk(KERN_INFO "[KP] control: setting up inline hook ...\n");
+        printk(KERN_INFO "[KP] control: enabling inline hook\n");
         
-        // 如果已经有钩子，先清理
+        // 清理已有钩子
         if (hook_type == FUNCTION_POINTER_CHAIN) {
             fp_unhook_syscalln(__NR_openat, before_openat_0, 0);
             fp_unhook_syscalln(__NR_openat, before_openat_1, after_openat_1);
@@ -147,7 +153,7 @@ static long syscall_hook_control0(const char *args, char *__user out_msg, int ou
         err = inline_hook_syscalln(__NR_openat, 4, before_openat_0, 0, 0);
         
     } else if (!strcmp("unhook", args)) {
-        printk(KERN_INFO "[KP] control: removing hooks ...\n");
+        printk(KERN_INFO "[KP] control: removing hooks\n");
         
         if (hook_type == FUNCTION_POINTER_CHAIN) {
             fp_unhook_syscalln(__NR_openat, before_openat_0, 0);
@@ -160,11 +166,11 @@ static long syscall_hook_control0(const char *args, char *__user out_msg, int ou
         return 0;
         
     } else if (!strcmp("status", args)) {
-        printk(KERN_INFO "[KP] control: Status - Hook type: %d\n", hook_type);
+        printk(KERN_INFO "[KP] control: Hook type:%d\n", hook_type);
         return 0;
         
     } else {
-        printk(KERN_WARNING "[KP] control: unknown args: %s\n", args);
+        printk(KERN_WARNING "[KP] control: unknown command:%s\n", args);
         printk(KERN_INFO "[KP] control: Available commands:\n");
         printk(KERN_INFO "[KP] control:   function_pointer_hook - Enable function pointer hook\n");
         printk(KERN_INFO "[KP] control:   inline_hook - Enable inline hook\n");
@@ -175,24 +181,23 @@ static long syscall_hook_control0(const char *args, char *__user out_msg, int ou
 
 out_control:
     if (err) {
-        printk(KERN_ERR "[KP] control: hook openat error: %d\n", err);
+        printk(KERN_ERR "[KP] control: hook error:%d\n", err);
         return -1;
     } else {
-        printk(KERN_INFO "[KP] control: hook openat success\n");
+        printk(KERN_INFO "[KP] control: hook success\n");
         return 0;
     }
 }
 
 static long syscall_hook_demo_exit(void *__user reserved)
 {
-    printk(KERN_INFO "[KP] kpm-syscall-hook-demo exit ...\n");
+    printk(KERN_INFO "[KP] kpm-syscall-hook-demo exit\n");
 
     if (hook_type == INLINE_CHAIN) {
         inline_unhook_syscalln(__NR_openat, before_openat_0, 0);
     } else if (hook_type == FUNCTION_POINTER_CHAIN) {
         fp_unhook_syscalln(__NR_openat, before_openat_0, 0);
         fp_unhook_syscalln(__NR_openat, before_openat_1, after_openat_1);
-    } else {
     }
     return 0;
 }
